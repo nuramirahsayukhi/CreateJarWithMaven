05ae2f6cdfc62b541ad798cb5c67d5d2c3b1bc23
nuramirahsayukhi <nuramirahsayukhi@gmail.com>
2021-04-11 11:14:02 +0800

    Initial commit

---
+++ HelloWorld/src/main/java/my/amirah/App.java
@@ -0,0 +1,25 @@
+package my.amirah;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame
+{
+    public App () {
+        setTitle("Hello World");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+
+    }
+
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
